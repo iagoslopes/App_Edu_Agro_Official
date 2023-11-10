48f0e68a3cb02ed324e3ca6c9c164b0a8b5da141
Iago <iagodesouza2012@gmail.com>
2023-11-10 19:43:12 -0300

    Mudando o sobre

--- web-app-edu-agro/src/pages/Sobre/index.jsx
+++ web-app-edu-agro/src/pages/Sobre/index.jsx
@@ -15,15 +15,21 @@ export const Sobre = () => {
                     </div>
                     <div className="missao">
                         <p>Nossa Missão</p>
-                        <span>Levar conhecimento do mundo agro para pessoas entusiastas</span>
+                        <span>O nosso objetivo é levar conhecimento de forma acessível para instruir entusiastas, 
+                            curiosos e estudantes sobre o mundo agro. Oferecemos um compêndio vasto com informações 
+                            de inúmeras espécimes para possibilitar o cultivo, independente de sua idade ou classe social. 
+                            Nossa filosofia é de que, alimentando a cultura do cultivo, podemos incentivar uma alimentação 
+                            mais saudável e também um hobby relaxante.</span>
                     </div>
                 </div>
                 <div className="espaco"></div>
                 <div className="historia-container">
                     <div className="historia">
                         <p>Nossa História</p>
-                        <span>Nosso projeto nasceu de um projeto interdisciplinar no 
-                            intuito de elaborar um site e um aplicativo mobile
+                        <span>O Education Agro nasceu de um projeto interdisciplinar no intuito de elaborar um site e um 
+                            aplicativo mobile. Um tema recorrente durante nossos estudos foi a aplicação da tecnologia 
+                            na agricultura, então nos surgiu uma ideia: desenvolver uma biblioteca pública e gratuita com 
+                            todas as informações necessárias para cultivar as mais diversas flores, raízes, vegetais, legumes e mais.
                         </span>
                     </div>
                     <div className="historia-imagem">
